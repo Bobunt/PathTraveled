83f083f2ec17ad9e48202efbfc2d65f614b1fe29
Musta <daniil.cherrnyi@gmail.com>
2023-04-09 14:11:37 +0300

    нашел пример. Буду делать опираяь на него

--- app/src/main/java/com/example/pathtraveled/navigator/mapFragment.kt
+++ app/src/main/java/com/example/pathtraveled/navigator/mapFragment.kt
@@ -1,22 +1,34 @@
 package com.example.pathtraveled.navigator
 
-import android.annotation.SuppressLint
+
+
+import android.Manifest
+import android.content.pm.PackageManager
 import android.os.Bundle
-import android.os.Parcel
-import android.os.Parcelable
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.example.pathtraveled.R
-import com.example.pathtraveled.databinding.FragmentMainBinding
+import android.widget.EditText
+import androidx.core.app.ActivityCompat
 import com.example.pathtraveled.databinding.FragmentMapBinding
+import com.yandex.mapkit.Animation
+import com.yandex.mapkit.MapKit
 import com.yandex.mapkit.MapKitFactory
 import com.yandex.mapkit.geometry.Point
-import com.yandex.mapkit.map.CameraPosition
+import com.yandex.mapkit.layers.ObjectEvent
+import com.yandex.mapkit.map.*
+import com.yandex.mapkit.map.Map
 import com.yandex.mapkit.mapview.MapView
+import com.yandex.mapkit.search.Response
+import com.yandex.mapkit.search.SearchManager
+import com.yandex.mapkit.search.SearchOptions
+import com.yandex.mapkit.search.Session
+import com.yandex.mapkit.user_location.UserLocationObjectListener
+import com.yandex.mapkit.user_location.UserLocationView
+import com.yandex.runtime.Error
 
-class mapFragment : Fragment() {
+class mapFragment : Fragment(), UserLocationObjectListener, Session.SearchListener, CameraListener{
     lateinit var mapView: MapView
     private var _binding: FragmentMapBinding? = null
     private val binding get() = _binding!!
@@ -34,9 +46,15 @@ class mapFragment : Fragment() {
     ): View? {
         _binding = FragmentMapBinding.inflate(inflater, container, false)
         mapView = binding.mapview
-        mapView.map.move(CameraPosition(Point(47.0,39.0),11.0f,0.0f,0.0f))
-
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        mapView.map.move(CameraPosition(Point(47.219775,39.718409),11.0f,0.0f,0.0f),
+        Animation(Animation.Type.SMOOTH, 10f), null)
+        var mapKit: MapKit = MapKitFactory.getInstance()
+        requestLocationPremision()
     }
 
     override fun onStart() {
@@ -49,5 +67,44 @@ class mapFragment : Fragment() {
         mapView.onStop()
         MapKitFactory.getInstance().onStop()
         super.onStop()
+    }
+
+    private fun requestLocationPremision(){
+        if(
+            ActivityCompat.checkSelfPermission(this.requireContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED ||
+            ActivityCompat.checkSelfPermission(this.requireContext(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED)
+        {
+            ActivityCompat.requestPermissions(this.requireActivity(), arrayOf(Manifest.permission.ACCESS_FINE_LOCATION,Manifest.permission.ACCESS_COARSE_LOCATION), 0)
+            return
+        }
+    }
+
+    override fun onObjectAdded(p0: UserLocationView) {
+
+    }
+
+    override fun onObjectRemoved(p0: UserLocationView) {
+        TODO("Not yet implemented")
+    }
+
+    override fun onObjectUpdated(p0: UserLocationView, p1: ObjectEvent) {
+        TODO("Not yet implemented")
+    }
+
+    override fun onSearchResponse(p0: Response) {
+        TODO("Not yet implemented")
+    }
+
+    override fun onSearchError(p0: Error) {
+        TODO("Not yet implemented")
+    }
+
+    override fun onCameraPositionChanged(
+        p0: Map,
+        p1: CameraPosition,
+        p2: CameraUpdateReason,
+        p3: Boolean
+    ) {
+        TODO("Not yet implemented")
     }
 }
